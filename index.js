f762657270f31be5b1cacf2f8da18c0b60157535
RupinSamria <rupin.samria@gmail.com>
2020-02-14 17:30:16 +0530

    single reducer multiple action

--- index.js
+++ index.js
@@ -1,34 +1,51 @@
 const redux = require('redux')
 
-const BUY_CAKE = 'BUY_CAKE'; 
+const BUY_CAKE = 'BUY_CAKE';
+const BUY_ICECREAM = 'BUY_ICECREAM';
 const createStore = redux.createStore;
 
+//Action Creator
 function buyCake() {
     return {
         type: BUY_CAKE,
         info: 'first redux action'
     }
 }
-
+function buyIcecream() {
+    return{
+        type: BUY_ICECREAM,
+    }
+}
+//
 initialState = {
-    numOfCakes: 10
+    numOfCakes: 10,
+    numOfIcecream: 20
 }
 
+//Reducer
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case BUY_CAKE: return{
             ...state,
             numOfCakes: state.numOfCakes - 1
         }
+        case BUY_ICECREAM: return{
+            ...state,
+            numOfIcecream: state.numOfIcecream -1
+        }
     
         default: return state
     }
 }
 
+
+//Store
 const store = createStore(reducer)
 console.log('Initial State', store.getState())
 const unsubscribe = store.subscribe(() => console.log('Updated State', store.getState()))
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
+store.dispatch(buyIcecream())
+store.dispatch(buyIcecream())
 unsubscribe()
